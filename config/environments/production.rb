9676fe4469609f60dc39331ae4ee656d09c70dd9
robrieba <robrieba@gmail.com>
2015-09-19 15:42:23 -0400

    commit before deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: "FIXME", port: 3000 }
+  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
